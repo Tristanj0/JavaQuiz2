f3914e3af57471e59dc74d28ffd103e4c33e0512
Tristan <tristan@Zipcoders-MacBook-Pro-2.local>
2022-07-16 00:33:13 -0400

    Did merge in ArrayUtil

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,8 +1,18 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.ArrayList;
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        Integer[] intArr = new Integer[array1.length + array2.length];
+        for (int i = 0; i < array1.length; i++) {
+            intArr[i] = array1[i];
+        }
+        for (int j = array1.length; j < intArr.length; j++) {
+            intArr[j] = array2[j - array1.length];
+        }
+
+        return intArr;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
