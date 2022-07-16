01d205361c892d8a64d6e62fabe96152495c3ea8
Tristan <tristan@Zipcoders-MacBook-Pro-2.local>
2022-07-15 23:27:00 -0400

    Part 1 done

--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -1,19 +1,31 @@
 package com.zipcodewilmington.assessment2.part1;
 
 public class WuTangConcatenator {
+     Integer input;
     public WuTangConcatenator(Integer input) {
+        this.input = input;
+    }
+        public Boolean isWu () {
 
+            if (input % 3 == 0) {
+                return true;
+            }
+            return false;
+        }
+
+        public Boolean isTang () {
+            if (input % 5 == 0) {
+                return true;
+            }
+            return false;
+        }
+
+        public Boolean isWuTang () {
+            if (input % 15 == 0) {
+                return true;
+            }
+            return false;
+        }
     }
 
-    public Boolean isWu() {
-        return null;
-    }
 
-    public Boolean isTang() {
-        return null;
-    }
-
-    public Boolean isWuTang() {
-        return null;
-    }
-}
